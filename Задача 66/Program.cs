b8e8c33dddd931e47c41cc33885cb96aa78bcaae
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-10 16:16:01 +0300

    выполнили задачу 66

--- Задача 66/Program.cs
+++ Задача 66/Program.cs
@@ -7,3 +7,28 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 
 */
+
+int GetUserInput(string text) {
+    System.Console.Write(text);
+    int input = int.Parse(System.Console.ReadLine()!);
+    return input;
+}
+
+void PrintNumbers(int start, int end, int sum) {
+    if (start <= end) {
+        sum = sum + start;
+        start++;
+        PrintNumbers(start, end, sum);
+    }
+    else {
+        System.Console.WriteLine(sum);
+    }
+}
+ void Main() {
+    int start = GetUserInput("Введите первое число: ");
+    int end = GetUserInput("Введите второе число: ");
+    PrintNumbers(start, end, 0);
+ }
+
+ Main();
+
